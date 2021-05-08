7d4f6946981d812dc2a20d425afd7c23079422b7
m0nk3y2k4 <m0nk3y@t-online.de>
2021-05-08 20:27:53 +0200

    Re-enabled single movie integration test after endpoint has been fixed

--- src/integration-test/java/com/github/m0nk3y2k4/thetvdb/api/MoviesIT.java
+++ src/integration-test/java/com/github/m0nk3y2k4/thetvdb/api/MoviesIT.java
@@ -19,14 +19,12 @@ package com.github.m0nk3y2k4.thetvdb.api;
 import static com.github.m0nk3y2k4.thetvdb.testutils.assertj.IntegrationTestAssertions.assertThat;
 
 import com.github.m0nk3y2k4.thetvdb.testutils.annotation.IntegrationTestSuite;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 @IntegrationTestSuite("Movies")
 class MoviesIT {
 
-    @Disabled("Movies endpoint is currently broken on TheTVDB.com side, constantly returning HTTP-404")
     @Test
     @Order(2)
     void getMovie(TheTVDBApi api) {
