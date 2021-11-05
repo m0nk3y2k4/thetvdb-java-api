1246befa57d3af5ab4b128e6faa2b64671266eef
m0nk3y2k4 <m0nk3y@t-online.de>
2021-11-05 14:29:10 +0100

    Updated API version reference to the latest release 4.4.3
    The version number in the OpenAPI documentation seems to be outdated

--- src/main/java/com/github/m0nk3y2k4/thetvdb/api/TheTVDBApi.java
+++ src/main/java/com/github/m0nk3y2k4/thetvdb/api/TheTVDBApi.java
@@ -3238,7 +3238,7 @@ public interface TheTVDBApi {
      */
     final class Version {
         /** Version of the <i>TheTVDB.com</i> remote API used by this connector */
-        public static final String API_VERSION = "v4.4.0";
+        public static final String API_VERSION = "v4.4.3";
 
         /** Constant class. Should not be instantiated */
         private Version() {}
