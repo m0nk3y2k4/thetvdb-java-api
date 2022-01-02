64d2f6a35224fe168dbbb020131186aaf94a4cf9
m0nk3y2k4 <m0nk3y@t-online.de>
2022-01-02 14:28:53 +0100

    Updated copyright headers

--- src/test/java/com/github/m0nk3y2k4/thetvdb/internal/api/impl/model/data/CompanyTypeDTOTest.java
+++ src/test/java/com/github/m0nk3y2k4/thetvdb/internal/api/impl/model/data/CompanyTypeDTOTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 - 2021 thetvdb-java-api Authors and Contributors
+ * Copyright (C) 2019 - 2022 thetvdb-java-api Authors and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
